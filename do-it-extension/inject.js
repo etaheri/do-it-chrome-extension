2fc0a5d3faedc50103ec7ed5f8e188492100c4aa
erik taheri <etaheri@me.com>
2015-06-09 01:37:23 -0400

    added new video link and error prompt

--- do-it-extension/inject.js
+++ do-it-extension/inject.js
@@ -6,12 +6,15 @@ if(videoEl == null){
 	div.innerHTML = '<div class="container"><video height="540px" autoplay="" name="media"><source type="video/webm"></video></div>'
 	var video = document.querySelector('.shia-do-it .container video');
 	video.style.visibility = "hidden";
-	video.src = "http://taheri.io/output.webm";
+	video.src = "http://etaheri.github.io/output.webm";
 	video.load();
 	video.addEventListener('loadeddata', function() {
 	   video.style.visibility = "visible"
 	   video.play();
 	}, false);
+	video.onerror = function() {
+    	alert("Couldnt load Shia :( Try another page.");
+	};
 }
 else{
 	videoEl.play();
