2cf4032dbf2e7ec8d8dbf23e62ebb1571016fc2f
Daniel Hickman <danielhickman9@gmail.com>
2015-07-14 15:53:02 -0700

    Second Revision

--- do-it-extension/inject.js
+++ do-it-extension/inject.js
@@ -4,7 +4,7 @@ if (videoEl == null) {
 	document.body.appendChild(div);
 	div.className = 'shia-do-it';
 	div.style.cssText = ('position: fixed; bottom: 0px; z-index: 99999; right: 0px; height: 530px; width: 720px; pointer-events: none;');
-	div.innerHTML = '<div class="shia-do-it--container" style="width: 720px; overflow:hidden; display:block; height: 535px;"><video width="960" height="540" style="margin-top: -5px; max-width: none; padding: 0;" autoplay="" name="media"><source type="video/webm"></video></div>';
+	div.innerHTML = '<div class="shia-do-it--container" style="width: 720px; overflow:hidden; display:block; height: 535px;"><video width="960" height="540" style="margin-top: -5px; max-width: none; padding: 0;" name="media"><source type="video/webm"></video></div>';
 	var video = document.querySelector('.shia-do-it .shia-do-it--container video');
 	video.style.visibility = "hidden";
 	video.src = chrome.extension.getURL("/media/output.webm");
